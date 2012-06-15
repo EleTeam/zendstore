2c44bb3a5d4aa0d94768b3ab3844300db7db4a36
Huazai <hi.huazai@gmail.com>
2012-06-15 09:38:43 +0800

    Add UserModel

---
+++ module/User/src/User/Model/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace User\Model;
+
+use Zend\Db\ResultSet\Row;
+
+class User extends Row
+{
+	
+}
---
+++ module/User/src/User/Model/UserTable.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace User\Model;
+
+use Zend\Db\TableGateway\AbstractTableGateway,
+	Zend\Db\Adapter\Adapter,
+	Zend\Db\ResultSet\ResultSet;
+
+class UserTable extends AbstractTableGateway
+{
+	
+}
